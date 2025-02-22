cc10859d7e10e3a7ef57a23c8d3ff93e08830491
Cubsystem <42353514+acordobaj@users.noreply.github.com>
2025-02-22 12:24:41 -0600

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QApplication
-from ui.login_window import LoginWindow
-import sys
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    login_window = LoginWindow()  # No se pasa ningún argumento adicional
-    login_window.show()
-    sys.exit(app.exec_())
